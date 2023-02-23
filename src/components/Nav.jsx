dd03f8745551bae9bbe1512b5ab954539e90c31e
Nicholas Easter <nicholas.easter17@gmail.com>
2023-02-22 18:37:20 -0800

    updated nav component, has basic responsiveness with flex

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,17 @@
 export const Nav = () => {
 
     return (
-        <div>
-            <h1>Cash's Creatures</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a>Available Animals</a>
-                    </li>
-                    <li>
-                        <a>About</a>
-                    </li>
-                    <li>
-                        <a>Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="font-sans flex flex-col sm:flex-row items-center sm:justify-between bg-green-600">
+            {/* title */}
+            <div className="text-2xl p-2">
+                <a href="#">Cash's Creatures</a>
+            </div>
+            {/* links */}
+            <div className="flex my-auto">
+                <a href="#" className="text-lg no-underline mx-2 hover:text-cyan-400">Creatures</a>
+                <a href="#" className="text-lg no-underline mx-2 hover:text-cyan-400">About</a>
+                <a href="#" className="text-lg no-underline mx-2 hover:text-cyan-400">Contact</a>
+            </div>
+        </nav>
     )
 }
