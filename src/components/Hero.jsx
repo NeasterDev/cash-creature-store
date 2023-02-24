7e6efd8a32dd94fe385c20c2bdf03929673da0c0
Nicholas Easter <nicholas.easter17@gmail.com>
2023-02-23 20:12:14 -0800

    made the hero image more responsive, added the main text to the header with some styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
+import bgImage from '../assets/bg-image.jpg';
+
 export const Hero = () => {
 
     return (
-        <div>
-            <h1>Cash's Creatures</h1>
-            <p>We sell creatures for cash and take cash for creatures</p>
+        <div className="bg-hero w-full h-96 lg:h-screen bg-cover bg-center flex justify-center items-center">
+            <div className=' bg-white p-2'>
+                <h1 className='text-6xl font-bold'>Cash's Creatures</h1>
+                <p className='text-3xl italic'>We sell creatures for cash and take cash for creatures</p>
+            </div>
+            {/* <img src={bgImage} /> */}
         </div>
     )
 }
