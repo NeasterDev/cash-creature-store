31b63e25c78d476aa7eff901fa52937c22e406a1
NeasterDev <nicholas.easter17@gmail.com>
2023-02-28 13:34:52 -0800

    added basic contact form, need to finish styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,16 @@
 export const Contact = () => {
 
     return (
-        <div>
-            <h1>Contact me</h1>
+        <section className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center">
+                <h1>Contact Me</h1>
+                <form>
+                    <label className="mx-2" htmlFor="emailInput">Email</label>
+                    <input className="border-2 rounded-sm p-1 text-sm active:border-blue-300" name="emailInput" type="email" />
+                </form>
+            </div>
             <div>(000)-000-0000</div>
             <div>cashcreatures@gmail.com</div>
-        </div>
+        </section>
     )
 }
